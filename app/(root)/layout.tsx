35543890e6e51978e9b3c2c6977e85658877e1aa
Chiragoel27 <chiragoel27@gmail.com>
2024-06-08 11:31:33 +0530

    updated package

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,39 +4,6 @@ import React, { ReactNode, useEffect, useState } from "react";
 import StreamVideoProvider from "@/providers/streamClientProvider";
 import MyCallUI from "@/components/meeting/MyCallUI";
 import { useUser } from "@clerk/nextjs";
-
-const ClientRootLayout = ({ children }: { children: ReactNode }) => {
-	const { user } = useUser();
-	const [isMounted, setIsMounted] = useState(false);
-
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
-
-	if (!isMounted) {
-		return <>{children}</>;
-	}
-
-	return user ? (
-		<StreamVideoProvider>
-			<div className="relative min-h-screen w-full">
-				<MyCallUI />
-				{children}
-			</div>
-		</StreamVideoProvider>
-	) : (
-		<>{children}</>
-	);
-};
-
-export default ClientRootLayout;
-
-"use client";
-
-import React, { ReactNode, useEffect, useState } from "react";
-import StreamVideoProvider from "@/providers/streamClientProvider";
-import MyCallUI from "@/components/meeting/MyCallUI";
-import { useUser } from "@clerk/nextjs";
 import Loader from "@/components/shared/Loader";
 
 const ClientRootLayout = ({ children }: { children: ReactNode }) => {
