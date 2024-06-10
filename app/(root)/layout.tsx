e034d71cf8d71073d4b655bbcafeb102fd2ca9e8
Chiragoel27 <chiragoel27@gmail.com>
2024-06-10 17:22:07 +0530

    removed some errors

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import StreamVideoProvider from "@/providers/streamClientProvider";
 import Loader from "@/components/shared/Loader";
+import { WalletBalanceProvider } from "@/lib/context/WalletBalanceContext";
 
 const ClientRootLayout = ({ children }: { children: ReactNode }) => {
 	const [isMounted, setIsMounted] = useState(false);
@@ -16,9 +17,12 @@ const ClientRootLayout = ({ children }: { children: ReactNode }) => {
 	}
 
 	return (
+		<WalletBalanceProvider>
+
 		<StreamVideoProvider>
 			<div className="relative min-h-screen w-full">{children}</div>
 		</StreamVideoProvider>
+		</WalletBalanceProvider>
 	);
 };
 
