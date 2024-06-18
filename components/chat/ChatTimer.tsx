5daeec77ef30ef8d9aa0fdab9c9ce34b954eae88
Chiragoel27 <chiragoel27@gmail.com>
2024-06-18 17:22:54 +0530

    chat status

---
+++ components/chat/ChatTimer.tsx
@@ -0,0 +1,20 @@
+import { useChatTimerContext } from "@/lib/context/ChatTimerContext";
+
+const ChatTimer: React.FC = () => {
+    const { timeLeft } = useChatTimerContext();
+    const timeLeftInSeconds = parseFloat(timeLeft);
+	const isLoading = isNaN(timeLeftInSeconds) || timeLeftInSeconds <= 0;
+
+	const minutes = Math.floor(timeLeftInSeconds / 60);
+	const seconds = Math.floor(timeLeftInSeconds % 60)
+		.toString()
+		.padStart(2, "0");
+
+    return (
+        <div className="leading-5 text-center text-white font-bold py-1 bg-[rgba(255,255,255,0.36)] mb-4">
+            Time Left: {minutes}:{seconds}
+        </div>
+    )
+}
+
+export default ChatTimer;
