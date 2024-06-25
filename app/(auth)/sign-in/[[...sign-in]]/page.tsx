8e89463126c11915967124df1fdcdcf625a6a997
Aseem5047 <aseemgupta2002@gmail.com>
2024-06-25 17:38:14 +0530

    updated authentication pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SiginInPage() {
 	return (
-		<main className="flex h-screen w-full items-end sm:items-center justify-center bg-black/20 no-scrollbar">
-			<div className="animate-enterFromBottom sm:animate-none">
+		<main className="flex h-screen w-full items-end sm:items-center justify-center bg-black/20 no-scrollbar  overflow-hidden">
+			<div className="animate-enterFromBottom">
 				<SignIn />
 			</div>
 		</main>
