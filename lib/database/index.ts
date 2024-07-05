55cadb7e1dcada6ad1a7f4fc96161c77199862c7
Aseem5047 <aseemgupta2002@gmail.com>
2024-07-05 18:51:59 +0530

    updated

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
 	cached.promise =
 		cached.promise ||
 		mongoose.connect(MONGODB_URI, {
-			dbName: "unite",
+			dbName: "flash",
 			bufferCommands: false,
 		});
 
