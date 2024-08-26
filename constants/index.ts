c71f4c3b272816e9a1a24bd295689e788db0b031
Chiragoel27 <chiragoel27@gmail.com>
2024-08-26 14:49:19 +0530

    refer link changes

--- constants/index.ts
+++ constants/index.ts
@@ -38,6 +38,11 @@ export const sidebarLinksCreator = [
 		label: "KYC",
 	},
 	{
+		imgURL: "/icons/refer-and-earn.svg",
+		route: "/refer",
+		label: "Refer and Earn",
+	},
+	{
 		imgURL: "/icons/support.png",
 		route: "/support",
 		label: "Support",
