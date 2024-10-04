707cd14fe78e8ed314319af7b5448930f7b27c12
Aseem5047 <aseemgupta2002@gmail.com>
2024-10-04 16:20:52 +0530

    resolved issues

--- components/forms/AuthenticateViaOTP.tsx
+++ components/forms/AuthenticateViaOTP.tsx
@@ -188,7 +188,8 @@ const AuthenticateViaOTP = ({
 						audioRate: "0",
 						chatRate: "0",
 						walletBalance: 0,
-						referredBy: refId ? refId : null,
+						referredBy: refId ? refId : "",
+						referralAmount: refId ? 5000 : 0,
 						creatorId: `@${formattedPhone as string}`,
 					};
 				} else {
