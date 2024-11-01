ade5e186fdeccb7d56c497af3b5f7baf582aa9f5
Aseem5047 <aseemgupta2002@gmail.com>
2024-11-01 12:08:31 +0530

    updated changes

--- app/(root)/(home)/userFeedbacks/page.tsx
+++ app/(root)/(home)/userFeedbacks/page.tsx
@@ -1,24 +1,54 @@
 "use client";
 import CreatorCallsFeedbacks from "@/components/creator/CreatorCallsFeedbacks";
+import SinglePostLoader from "@/components/shared/SinglePostLoader";
 import { useCurrentUsersContext } from "@/lib/context/CurrentUsersContext";
+import { useGetFeedbacks } from "@/lib/react-query/queries";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 const UserFeedbacks = () => {
-	const { userType, creatorURL } = useCurrentUsersContext();
+	const { userType, creatorURL, creatorUser } = useCurrentUsersContext();
 	const router = useRouter();
+	const { ref, inView } = useInView({
+		threshold: 0.1,
+		triggerOnce: false,
+	});
+
+	const {
+		data: feedbackData,
+		isLoading,
+		isFetching,
+		isError,
+		fetchNextPage,
+		hasNextPage,
+	} = useGetFeedbacks(creatorUser?._id as string);
+
 	useEffect(() => {
-		// Check if the user type is 'creator'
 		if (userType !== "creator") {
-			// If creatorURL is present, route to that URL
 			if (creatorURL) {
 				router.push(`/${creatorURL}`);
 			} else {
-				// Otherwise, route to '/home'
 				router.push("/home");
 			}
 		}
 	}, [userType, creatorURL, router]);
+
+	useEffect(() => {
+		if (inView && hasNextPage && !isFetching) {
+			fetchNextPage();
+		}
+	}, [inView, hasNextPage, isFetching]);
+
+	if (isLoading) {
+		return (
+			<section className="w-full h-full flex items-center justify-center">
+				<SinglePostLoader />
+			</section>
+		);
+	}
+
 	return (
 		<section className=" flex size-full flex-col gap-2 pb-5">
 			<section
@@ -26,7 +56,32 @@ const UserFeedbacks = () => {
 			>
 				<h1 className="text-3xl font-bold">User Feedbacks</h1>
 			</section>
-			<CreatorCallsFeedbacks />
+			{isError ? (
+				<div className="size-full flex flex-col items-center justify-center text-2xl font-semibold text-center text-red-500">
+					Failed to fetch creators
+					<span className="text-lg">Please try again later.</span>
+				</div>
+			) : (
+				<CreatorCallsFeedbacks feedbackData={feedbackData} />
+			)}
+
+			{hasNextPage && isFetching && (
+				<Image
+					src="/icons/loading-circle.svg"
+					alt="Loading..."
+					width={50}
+					height={50}
+					className="mx-auto invert my-5 mt-10 z-20"
+				/>
+			)}
+
+			{!hasNextPage && !isFetching && creatorUser && (
+				<div className="text-center text-gray-500 py-4">
+					You have reached the end of the list
+				</div>
+			)}
+
+			{hasNextPage && <div ref={ref} className="pt-10 w-full" />}
 		</section>
 	);
 };
