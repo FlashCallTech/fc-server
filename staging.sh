b4bcdd4e3e31e6914adc34793a4dff9a30e44194
Aseem5047 <aseemgupta2002@gmail.com>
2024-11-29 16:53:46 +0530

    updated changes

--- staging.sh
+++ staging.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+echo "Running script for branch: $VERCEL_GIT_COMMIT_REF"
 
-# Check if the current branch is not 'staging'
 if [ "$VERCEL_GIT_COMMIT_REF" != "staging" ]; then
   echo "Skipping deploy for branch $VERCEL_GIT_COMMIT_REF"
-  exit 0  # Exits the script and skips the deployment
+  exit 0
 fi
 
-# If it's the 'staging' branch, continue with the deployment (optional)
 echo "Deploying to staging branch"
