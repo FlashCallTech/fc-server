40d971877030f2d24dd3d5433d0344c0dd30e7d1
Chiragoel27 <chiragoel27@gmail.com>
2024-12-04 21:21:02 +0530

    updates

--- components/chat/ChatInterface.tsx
+++ components/chat/ChatInterface.tsx
@@ -403,43 +403,52 @@ const ChatInterface: React.FC = () => {
 		}
 	}
 
+	useEffect(() => {
+		const handleResize = () => {
+			const height = window.innerHeight;
+			document.documentElement.style.setProperty("--vh", `${height * 0.01}px`);
+		};
+
+		window.addEventListener("resize", handleResize);
+		handleResize();
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
-		<div className={`relative flex flex-col h-screen justify-between bg-white w-screen overflow-y-auto scrollbar-hide`}>
-			<div className="sticky size-full top-0 left-0 h-screen">
-				<Image src={"/back.png"} alt="background" width={1000} height={1000} className="object-cover size-full" />
-			</div>
+		<div className={`flex flex-col h-screen justify-between w-screen bg-cover bg-center overflow-y-auto scrollbar-hide`} style={{ backgroundImage: 'url(/back.png)' }} >
 			<div className="fixed top-0 left-0 w-full flex justify-between items-center px-4 py-[2px] bg-gray-500 z-30">
 				<div className="flex items-center gap-2">
 					<Image
-						src={`${user2?.photo
-							? user2?.photo
-							: "https://firebasestorage.googleapis.com/v0/b/flashcallchat.appspot.com/o/assets%2FM_preview.png?alt=media&token=750fc704-c540-4843-9cbd-bfc4609780e0"
-							}`}
+						src={`${user2?.photo ? user2?.photo : 'https://firebasestorage.googleapis.com/v0/b/flashcallchat.appspot.com/o/assets%2FM_preview.png?alt=media&token=750fc704-c540-4843-9cbd-bfc4609780e0'}`}
 						alt="profile"
 						width={1000}
 						height={1000}
 						className="size-10 min-w-10 rounded-full object-cover"
 					/>
 					<div className="flex flex-col">
-						<div className="text-white font-bold text-xs md:text-sm">
-							{user2?.fullName
-								? user2?.fullName
-								: maskPhoneNumber(user2?.phone as string)}
+						<div className="text-white font-bold text-xs md:text-lg">
+							{user2?.fullName ? user2?.fullName : maskPhoneNumber(user2?.phone as string)}
 						</div>
 						{userType === "client" && <ChatTimer />}
 						{userType === "creator" && (
 							<CreatorChatTimer chatId={chatId as string} />
 						)}
-						<p className="text-[10px] md:text-xs text-green-500">
+						<p className="text-[10px] md:text-sm text-green-500">
 							Ongoing chat
 						</p>
 					</div>
 				</div>
 				<div className="flex gap-2">
-					<Tip handleSendTip={handleSendTip} setText={setText} />
+					<Tip
+						handleSendTip={handleSendTip}
+						setText={setText}
+					/>
 					<button
 						onClick={endCall}
-						className="bg-[rgba(255,81,81,1)] px-4 text-white text-[10px] md:text-sm rounded-lg"
+						className="bg-[rgba(255,81,81,1)] text-white p-2 md:px-4 md:py-2 text-[10px] md:text-lg rounded-lg"
 					>
 						End
 					</button>
@@ -451,35 +460,32 @@ const ChatInterface: React.FC = () => {
 					setShowDialog={handleCloseDialog}
 				/>
 			)}
-			<div className="absolute top-[50px] flex flex-1">
+			<div className="mt-auto pt-[50px]">
 				{/* Chat Messages */}
-				<div className="mb-[70px] mt-auto flex flex-1">
+				<div className="mb-[56px] z-20">
 					<Messages chat={chat!} img={img} isImgUploading={isImgUploading} />
 				</div>
 
-			</div>
-			{/* Sticky Chat Input at the Bottom */}
-			<div
-				className="fixed bottom-0 w-full z-30 bg-cover bg-center p-safe-bottom"
-				style={{ backgroundImage: "url(/back.png)" }}
-			>
-				<ChatInput
-					isRecording={isRecording}
-					discardAudio={discardAudio}
-					text={text}
-					setText={setText}
-					handleImg={handleImg}
-					handleSend={handleSend}
-					toggleRecording={toggleRecording}
-					img={img}
-					audio={audio}
-					audioStream={audioStream!}
-					// audioContext={audioContext}
-					handleCapturedImg={handleCapturedImg}
-					isImgUploading={isImgUploading}
-					isAudioUploading={isAudioUploading}
-					discardImage={discardImage}
-				/>
+				{/* Sticky Chat Input at the Bottom */}
+				<div className="fixed bottom-0 w-full z-30 bg-cover bg-center p-safe-bottom" style={{ backgroundImage: 'url(/back.png)' }}>
+					<ChatInput
+						isRecording={isRecording}
+						discardAudio={discardAudio}
+						text={text}
+						setText={setText}
+						handleImg={handleImg}
+						handleSend={handleSend}
+						toggleRecording={toggleRecording}
+						img={img}
+						audio={audio}
+						audioStream={audioStream!}
+						// audioContext={audioContext}
+						handleCapturedImg={handleCapturedImg}
+						isImgUploading={isImgUploading}
+						isAudioUploading={isAudioUploading}
+						discardImage={discardImage}
+					/>
+				</div>
 			</div>
 		</div>
 	);
