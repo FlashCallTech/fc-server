c0edba5cdf63dda3d97d7054c23814509786c4cf
Aseem5047 <aseemgupta2002@gmail.com>
2024-12-27 18:54:39 +0530

    updated changes

--- app/(root)/(home)/chatDetails/page.tsx
+++ app/(root)/(home)/chatDetails/page.tsx
@@ -1,41 +1,39 @@
 // pages/chatDetails.tsx
 
-'use client'
+"use client";
 
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import ChatDetails from "@/components/chat/ChatDetails";
 import { SelectedChat } from "@/types";
-import { trackEvent } from '@/lib/mixpanel';
-import { useCurrentUsersContext } from '@/lib/context/CurrentUsersContext';
+import { trackEvent } from "@/lib/mixpanel";
+import { useCurrentUsersContext } from "@/lib/context/CurrentUsersContext";
 
 const ChatDetailsPage = () => {
-    const [creatorId, setCreatorId] = useState<string | null>(null);
-    const { clientUser } = useCurrentUsersContext();
+	const [creatorId, setCreatorId] = useState<string | null>(null);
+	const { clientUser } = useCurrentUsersContext();
 
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        const creatorId = params.get('creatorId');
+	useEffect(() => {
+		const params = new URLSearchParams(window.location.search);
+		const creatorId = params.get("creatorId");
 
-        setCreatorId( creatorId );
-    }, []);
+		setCreatorId(creatorId);
+	}, []);
 
-    useEffect(() => {
-        trackEvent('OrderHistory_Profile_Clicked', {
-            Client_ID: clientUser?._id,
-			User_First_Seen: clientUser?.createdAt?.toString().split('T')[0],
+	useEffect(() => {
+		trackEvent("OrderHistory_Profile_Clicked", {
+			Client_ID: clientUser?._id,
+			User_First_Seen: clientUser?.createdAt?.toString().split("T")[0],
 			Creator_ID: creatorId,
 			Walletbalace_Available: clientUser?.walletBalance,
-        })
-    }, [])
+		});
+	}, []);
 
-    if (!creatorId) {
-        return <div>Loading...</div>; // Show a loading indicator or message
-    }
+	if (!creatorId) {
+		return <div>Loading...</div>; // Show a loading indicator or message
+	}
 
-    return (
-        < ChatDetails creatorId={creatorId? creatorId: null} />
-    );
-}
+	return <ChatDetails creatorId={creatorId ? creatorId : null} />;
+};
 
 export default ChatDetailsPage;
