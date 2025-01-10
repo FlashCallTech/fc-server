af8b371bb3f895960ce9813bcebcb0400b5c85b1
Aseem5047 <aseemgupta2002@gmail.com>
2025-01-10 13:35:52 +0530

    updated changes

--- app/(root)/(home)/profile/[userId]/page.tsx
+++ app/(root)/(home)/profile/[userId]/page.tsx
@@ -20,7 +20,10 @@ const UserProfilePage = () => {
 			(currentUser?.firstName ?? "") + " " + (currentUser?.lastName ?? ""),
 		firstName: currentUser?.firstName ?? "",
 		lastName: currentUser?.lastName ?? "",
-		username: currentUser?.username ?? "",
+		username:
+			currentUser?.username === currentUser?.phone
+				? currentUser?._id || `${currentUser?._id}`
+				: currentUser?.username || `${currentUser?._id}`,
 		profession: currentUser?.profession ?? "",
 		themeSelected: currentUser?.themeSelected ?? "#88D8C0",
 		phone: currentUser?.phone ?? "",
