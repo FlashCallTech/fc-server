c1b3b4e296e0d5c93a32e3aa0e5aff3b22b39a8a
Aseem5047 <aseemgupta2002@gmail.com>
2025-01-22 23:59:38 +0530

    updated changes

--- components/availabilityServices/UserAvailability.tsx
+++ components/availabilityServices/UserAvailability.tsx
@@ -428,11 +428,11 @@ const UserAvailability = ({ data, userId }: { data: any; userId: string }) => {
 													?.length > 0 ? (
 													watch(`weeklyAvailability.${dayIndex}.slots`)?.map(
 														(slot, slotIndex) => (
-															<section className="w-full flex flex-col gap-2 items-start">
-																<div
-																	key={slotIndex}
-																	className="w-full flex items-center gap-2 mb-2"
-																>
+															<section
+																key={slotIndex}
+																className="w-full flex flex-col gap-2 items-start"
+															>
+																<div className="w-full flex items-center gap-2 mb-2">
 																	{/* Start Time */}
 																	<FormField
 																		control={form.control}
