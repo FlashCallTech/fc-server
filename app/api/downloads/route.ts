e0482333a8dfb3e368e4cc3a180570968e2d0ea7
Aseem5047 <aseemgupta2002@gmail.com>
2025-03-17 12:56:15 +0530

    updated changes

--- app/api/downloads/route.ts
+++ app/api/downloads/route.ts
@@ -1,8 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	const s3Url =
-		"https://s3.eu-north-1.amazonaws.com/flashcall.me/downloads/official.apk";
+	const s3Url = "https://dxvnlnyzij172.cloudfront.net/downloads/official.apk";
 
 	// Redirect the user to the S3 URL
 	res.redirect(302, s3Url);
