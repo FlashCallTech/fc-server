df5c017bf5de988d825fb705b10e77ce89951da1
FlashCallTech <tech-admins@flashcall.me>
2025-03-17 13:05:15 +0530

    Delete app/api/downloads directory

--- app/api/downloads/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	const s3Url = "https://dxvnlnyzij172.cloudfront.net/downloads/official.apk";
-
-	// Redirect the user to the S3 URL
-	res.redirect(302, s3Url);
-}
